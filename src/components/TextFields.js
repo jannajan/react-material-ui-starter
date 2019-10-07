599a91faef418b88e2f691c84c936d0dc52018fd
jannajan <janj58102@gmail.com>
2019-10-06 22:21:19 -0500

    Add initial material ui text field

---
+++ src/components/TextFields.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+}))
+
+const TextFields = () => {
+  const classes = useStyles()
+
+  const [name, setName] = React.useState(``)
+
+  const handleChange = () => event => {
+    setName(event.target.value)
+  }
+
+  return (
+    <TextField style={{padding: 24}}
+      id="standard-input"
+      label="Name"
+      className={classes.textField}
+      value={name}
+      onChange={handleChange()}
+      margin="normal"
+    />
+  )
+}
+
+// TextFields.propTypes = {
+//   isDefault: PropTypes.bool,
+// }
+export default TextFields;
